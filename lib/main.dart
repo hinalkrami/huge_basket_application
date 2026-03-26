922bdbefa92adc21d194813f9e1e1bad1897a731
hinalkrami <hinalrami2004@gmail.com>
2026-03-26 11:39:28 +0530

    old files from date 20/3/2026

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,14 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_ce/hive.dart';
+import 'package:hive_ce_flutter/adapters.dart';
 import 'package:new_app/core/config/app_route.dart';
 import 'package:new_app/core/utils/app_colors.dart';
 import 'package:new_app/features/profile_page/data/manage_address_data_model/address_model.dart';
 import 'package:new_app/generated/l10n.dart';
-import 'package:path_provider/path_provider.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final appDocumentDir = Platform.isAndroid ? await getApplicationDocumentsDirectory() : await getLibraryDirectory();
-  Hive.init(appDocumentDir.path);
+  Hive.initFlutter();
   Hive.registerAdapter(AddressModelAdapter());
   await Hive.openBox<AddressModel>('addressBox');
   await Hive.openBox('userBox');
