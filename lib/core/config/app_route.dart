1f9f9f8523e4802383c3a2d9fc93cf887744d703
hinalkrami <hinalrami2004@gmail.com>
2026-03-26 11:26:19 +0530

    old files from date 20/3/2026

--- lib/core/config/app_route.dart
+++ lib/core/config/app_route.dart
@@ -4,13 +4,9 @@ import 'package:new_app/features/home_page/presentation/main_home_page.dart';
 import 'package:new_app/features/profile_page/presentation/manage_address/add_address.dart';
 import 'package:new_app/features/profile_page/presentation/manage_address/manage_address_page.dart';
 import 'package:new_app/features/my_orders_page/presentation/my_order_page.dart';
-import 'package:new_app/features/product_details_page/presentation/product_details_page.dart';
-import 'package:new_app/features/product_listing_page/presentation/product_listing_page.dart';
-import 'package:new_app/features/profile_page/presentation/profile_page.dart';
 import 'package:new_app/features/shop_details_page/presentation/shop_details_page.dart';
 import '../../features/auth/presentation/login_page.dart';
 import '../../features/category_subcategory_page/presentation/category_subcategory_page.dart';
-import '../../features/home_page/presentation/home_page.dart';
 import '../../features/user_details_page/presentation/user_details_page.dart';
 import '../../features/walk_through/presentation/walk_through_page.dart';
 
