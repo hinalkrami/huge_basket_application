79a8c9121c019fa70553e7dcf34a2bdc5b567c34
hinalkrami <hinalrami2004@gmail.com>
2026-03-27 10:16:34 +0530

    old files from date 20/3/2026

---
+++ lib/features/auth/data/model/user_details_model.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_details_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class UserDetailsModelAdapter extends TypeAdapter<UserDetailsModel> {
+  @override
+  final typeId = 0;
+
+  @override
+  UserDetailsModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return UserDetailsModel(
+      userNumber: fields[0] as String?,
+      zipCode: fields[5] as String?,
+      businessName: fields[1] as String?,
+      emailAdd: fields[4] as String?,
+      firstName: fields[2] as String?,
+      lastName: fields[3] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, UserDetailsModel obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.userNumber)
+      ..writeByte(1)
+      ..write(obj.businessName)
+      ..writeByte(2)
+      ..write(obj.firstName)
+      ..writeByte(3)
+      ..write(obj.lastName)
+      ..writeByte(4)
+      ..write(obj.emailAdd)
+      ..writeByte(5)
+      ..write(obj.zipCode);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is UserDetailsModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
